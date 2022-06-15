ca39f7806cb8071a3a56cdcaea54407356caa2a6
wildfirez98 <james@areafamily.us>
2022-06-15 18:50:32 -0500

    Updated with request and redirect for facts

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,4 +1,4 @@
-from flask import ( Blueprint, render_template )
+from flask import ( Blueprint, redirect, render_template, request )
 
 # Create the Blueprint instance - Blueprint does our endpoint routing so this will be http://127.0.0.1:5000/facts
 
@@ -7,3 +7,13 @@ bp = Blueprint('fact', __name__, url_prefix="/facts")
 @bp.route('/new') # Point Blueprint to /facts/new.html and our form
 def new():
     return render_template('facts/new.html')
+
+@bp.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        print(request.form)
+        return redirect('/facts')
+
+    print(request.form) # Use request object to access user given data on form. Be sure and import request from flask
+    return render_template('facts/index.html')
+
